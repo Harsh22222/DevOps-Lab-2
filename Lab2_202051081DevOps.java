b24a0a9d1a6d8c3792e8211646dad3de5cd6d5f9
Harsh Dawda <harshdawda786@gmail.com>
2023-01-18 16:34:39 +0530

    java code for devOps lab2(jenkins setup!)

--- Lab2_202051081DevOps.java
+++ Lab2_202051081DevOps.java
@@ -1,4 +1,4 @@
-public class Lab2_202051081DevOps(){
+public class Lab2_202051081DevOps{
 public static void main(String[] args) {
     System.out.println("Harsh Dawda 202051081 DevOps Lab-2 Assignment");
 }
