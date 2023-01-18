dff2c2db83017d698dc71924c6204a57e4adbcbb
Harsh Dawda <harshdawda786@gmail.com>
2023-01-18 17:00:10 +0530

    java code for devOps lab2(jenkins setup!)

--- Lab2_202051081DevOps.java
+++ Lab2_202051081DevOps.java
@@ -1,5 +1,5 @@
 public class Lab2_202051081DevOps{
 public static void main(String[] args) {
-    System.out.println("Harsh Dawda 202051081 DevOps Lab-2 Assignment, This executed Job-4 (refelect on change)");
+    System.out.println("Harsh Dawda 202051081 DevOps Lab-2 Assignment, This executed Job-5 (refelect changes in Jenkins while code is pushed using github webhook)");
 }
 }
